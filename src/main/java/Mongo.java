1a1ec30fc8a537d648da5fcc9a1303731a3b17c7
elauser <1810653024@stud.fh-kufstein.ac.at>
2020-08-13 07:33:42 +0900

    RiotUrlBuilder added

--- src/main/java/Mongo.java
+++ src/main/java/Mongo.java
@@ -1,22 +1,11 @@
-import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class Mongo {
 
     static final MongoClient mongoClient = MongoClients.create(HiddenConstants.CONNECTION_STRING);
     static final MongoDatabase database = mongoClient.getDatabase("lol");
-
-    public static void main(String[] args) {
-
-    }
-
-
-
-    /*
-    static MongoClient mongoClient = MongoClients.create(HiddenConstants.CONNECTION_STRING);
-    static MongoDatabase mongoDatabase = mongoClient.getDatabase("lol");
-    */
 
 }
---
+++ src/main/java/Riot.java
@@ -0,0 +1,36 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Riot {
+    static URL getSummonerUrlByName(String region, String summonerName) throws MalformedURLException {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("https://");
+        stringBuilder.append(region.toLowerCase());
+        stringBuilder.append(".api.riotgames.com/lol/summoner/v4/summoners/by-name/");
+        stringBuilder.append(summonerName);
+        stringBuilder.append("?api_key=" + HiddenConstants.RIOT_API_KEY);
+        return new URL(stringBuilder.toString());
+    }
+
+    static URL getSummonerUrlByAccountID(String region, String summonerId) throws MalformedURLException {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("https://");
+        stringBuilder.append(region.toLowerCase());
+        stringBuilder.append(".api.riotgames.com/lol/summoner/v4/summoners/by-account/");
+        stringBuilder.append(summonerId);
+        stringBuilder.append("?api_key=" + HiddenConstants.RIOT_API_KEY);
+        return new URL(stringBuilder.toString());
+    }
+
+    static URL getMatchHistory(String region, String summonerID, int endIndex) throws MalformedURLException {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("https://");
+        stringBuilder.append(region.toLowerCase());
+        stringBuilder.append(".api.riotgames.com/lol/match/v4/matchlists/by-account/");
+        stringBuilder.append(summonerID);
+        stringBuilder.append("?endIndex=20");
+        stringBuilder.append("&api_key=" + HiddenConstants.RIOT_API_KEY);
+
+        return new URL(stringBuilder.toString());
+    }
+}
